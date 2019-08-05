e539ac56f0d3e2fc30e51bcfb5db4e1cee0d5a15
Minnaldo <minnaldo6602@gmail.com>
2019-08-06 01:24:21 +0900

    HW_10_getit

---
+++ work_java/.metadata/.plugins/org.eclipse.core.resources/.history/20/303c070995b700191da79c8afae53593
@@ -0,0 +1,8 @@
+package GGGG;
+
+public class DuplicateException extends Exception {
+	public DuplicateException() {
+		System.out.println("중복되었어!");
+	}
+
+}
---
+++ work_java/Programming_Plus/src/HW_10_Product/Refrigerator.java
@@ -0,0 +1,28 @@
+package HW_10_Product;
+
+public class Refrigerator extends Product{
+	private int liter;
+	
+	public Refrigerator() {
+	}
+	
+	public Refrigerator(String productNum, String name, int price, int quantity, int liter) {
+		super(productNum, name, price, quantity);
+		this.liter = liter;
+	}
+
+	public int getLiter() {
+		return liter;
+	}
+
+	public void setLiter(int liter) {
+		this.liter = liter;
+	}
+
+	@Override
+	public String toString() {
+		return "Refrigerator [liter=" + liter + ", getProductNum()=" + getProductNum() + ", getName()=" + getName()
+				+ ", getPrice()=" + getPrice() + ", getAmount()=" + getAmount() + "]";
+	}
+
+}
