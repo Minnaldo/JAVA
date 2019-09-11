69ee996a694009ab22455bc4b2e0e21bd45b55bf
조민 <minnaldo6602@gmail.com>
2019-09-11 16:03:07 +0900

    190911_commit

---
+++ SWEA/src/SWEA_D3/Flatten_D3_1208.java
@@ -0,0 +1,23 @@
+package SWEA_D3;
+
+import java.util.Scanner;
+
+public class Flatten_D3_1208 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+		for (int tc = 1; tc <= T; tc++) {
+			int num = sc.nextInt();
+			int[] arr = new int[num];
+			for(int i = 0; i < num; i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+			
+			
+		}
+	}
+
+}
