a45ff3781c0ae55b8d17261bacc18fd0b8b4d313
Minnaldo <minnaldo6602@gmail.com>
2019-10-01 00:44:48 +0900

    191001_codeup

---
+++ CodeUp/src/CodeUp19.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class CodeUp19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		System.out.println(str);
+		
+		
+	}
+
+}
---
+++ CodeUp/src/CodeUp29.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class CodeUp29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		
+		double num1 = sc.nextDouble();
+		
+		System.out.printf("%.11f", num1);
+	}
+
+}
