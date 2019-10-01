5b271ca73de56e6ea153395ea53f8aa4bc9dbdf9
조민 <minnaldo6602@gmail.com>
2019-10-01 17:57:17 +0900

    191001

--- CodeUp/src/codeup01.java
+++
@@ -1,9 +0,0 @@
-
-public class codeup01 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
-	}
-
-}
--- CodeUp/src/codeup07.java
+++
@@ -1,9 +0,0 @@
-
-public class codeup07 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("\"C:\\Download\\hello.cpp\"");
-	}
-
-}
